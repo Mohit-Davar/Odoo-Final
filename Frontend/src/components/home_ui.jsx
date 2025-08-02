aeaff46920cde9553813387a6a8c31a7b28b6111
greedy-wudpeckr <mudituiet@gmail.com>
2025-08-02 17:10:20 +0530

    fixes

--- Frontend/src/components/home_ui.jsx
+++ Frontend/src/components/home_ui.jsx
@@ -102,6 +102,10 @@ export const Header = () => {
 };
 
 export const HeroSection = () => {
+    const handleLogin = ()=>{
+    navigate("/login");
+  }
+
   return (
     <section className="px-4 sm:px-6 lg:px-8 py-16 sm:py-20 lg:py-24">
       <div className="max-w-7xl mx-auto">
