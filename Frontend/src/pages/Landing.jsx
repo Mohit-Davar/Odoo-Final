cdb0fe089f4f56a8077fa6e90ae44ad6e39975f4
greedy-wudpeckr <mudituiet@gmail.com>
2025-08-31 10:19:34 +0530

    Build Issue

--- Frontend/src/pages/Landing.jsx
+++ Frontend/src/pages/Landing.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Navigation, HeroSection, AboutSection, ServicesSection, FeaturesGrid, StatisticsSection, FinalCTASection } from '@/components/Landing.jsx';
-import { CustomStyles, LoadingAnimation } from '@/components/ui/Home_UI.jsx';
+import { CustomStyles, LoadingAnimation } from '@/components/ui/home_ui.jsx';
 import Footer from '@/components/Footer';
 
 export default function LandingPage() {
